0309ca1b09796e270ba081f1c5401b679de14306
Arun <aruner02@gmail.com>
2018-03-18 11:00:08 +0530

    Added registration & login forms

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,51 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from login.forms import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.views.decorators.csrf import csrf_protect
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+ 
+@csrf_protect
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1'],
+            email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/register/success/')
+    else:
+        form = RegistrationForm()
+    variables = RequestContext(request, {
+    'form': form
+    })
+ 
+    return render_to_response(
+    'registration/register.html',
+    variables,
+    )
+ 
+def register_success(request):
+    return render_to_response(
+    'registration/success.html',
+    )
+ 
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+ 
+@login_required
+def home(request):
+    return render_to_response(
+    'home.html',
+    { 'user': request.user }
+    )
 
 
 def post_list(request):
@@ -39,4 +84,3 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-
